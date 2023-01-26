a7b5c427fcae6ad7c6c189d7811b4eb8cf87f089
Soumyadeep Halder <34742234+soumyadeephalder@users.noreply.github.com>
2023-01-26 20:27:59 +0530

    Update apache2-ubuntu.sh

--- apache2-ubuntu.sh
+++ apache2-ubuntu.sh
@@ -16,12 +16,31 @@ then
 
 
     sudo apt install -y php8.1 libapache2-mod-php
-    sudo apt-get install -y php8.1-cli php8.1-json php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+    sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 
     sleep 2
 
 
     sudo chown -R $USER:$USER /var/www/html
+    
+    
+    wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.zip
+    
+    sleep 2
+    
+    sudo apt install unzip
+    
+    sleep 2
+    
+    unzip phpMyAdmin-5.2.0-all-languages.zip
+    mv phpMyAdmin-5.2.0-all-languages phpmyadmin
+    mv ~/phpmyadmin /var/www/html
+    rm -rf phpMyAdmin-5.2.0-all-languages.zip
+    
+    sudo chown -R $USER:$USER /var/www/html
+    sudo phpenmod mbstring
+    sudo systemctl restart apache2
+    
 
     sudo nano /etc/apache2/mods-enabled/dir.conf
 
