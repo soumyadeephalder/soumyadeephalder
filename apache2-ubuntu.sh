e1677e59903beeeb4c58f5c887aedfa2728f7922
Soumyadeep Halder <34742234+soumyadeephalder@users.noreply.github.com>
2023-01-31 13:22:14 +0530

    Update apache2-ubuntu.sh

--- apache2-ubuntu.sh
+++ apache2-ubuntu.sh
@@ -10,10 +10,14 @@ then
     
     sleep 2
 
+    sudo apt update
     sudo apt install mysql-server -y
 
     sleep 2
-
+    
+    sudo apt update && apt install -y software-properties-common 
+    sudo add-apt-repository ppa:ondrej/php 
+    sudo apt update
 
     sudo apt install -y php8.1 libapache2-mod-php
     sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
@@ -34,6 +38,7 @@ then
     
     unzip phpMyAdmin-5.2.0-all-languages.zip
     mv phpMyAdmin-5.2.0-all-languages phpmyadmin
+    rm -rf /var/www/html/phpmyadmin
     mv ~/phpmyadmin /var/www/html
     rm -rf phpMyAdmin-5.2.0-all-languages.zip
     
